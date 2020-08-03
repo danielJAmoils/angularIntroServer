0096b7e3b9479d4c494b328dca603c45362c3d4a
Daniel Amoils <danieljamoils@gmail.com>
2020-08-03 17:25:50 -0400

    registration part 3: mongoDB register and login

--- index.js
+++ index.js
@@ -1,15 +1,45 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+mongoose.Promise = Promise
+mongoose.connect('mongodb://localhost:27017/angulardb')
+.then(() => console.log('Mongoose up'))
+
+const User = require('./models/users')
 
 const app = express()
 
 app.use(bodyParser.json())
 
-app.post('/api/register', (req, res) => {
-    const {username, password} = req.body
+app.post('/api/login', async (req, res) => {
+    const {email, password} = req.body
 
-    //store this data on database
-    
+    console.log(email,password)
+
+    const resp = await User.findOne({email, password})
+
+    if(!resp){
+        console.log('Incorrect details')
+        //user login is incorrect 
+    }else{
+        console.log('Logging you in')
+        //make a session and set user to logged in
+    }
+    res.send('k')
+})
+
+app.post('/api/register', async (req, res) => {
+    const {email, password} = req.body
+    const user = new User({
+        email,
+        password
+    })
+
+    const result = await user.save()
+    console.log(result)
+
+    res.json(result)
 })
 
 app.listen(1234, () => {
---
+++ readme.md
@@ -0,0 +1,3 @@
+This is the node js express server for my intro to angular registration project
+
+If there are missing step there they should be located here
